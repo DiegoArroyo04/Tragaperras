99bee6d91f16cc669690ef263b7bff04ebec3537
DiegoArroyo04 <diegoarroyogonzalez04@gmail.com>
2024-11-05 21:07:12 +0100

    Primeros avances notables estilo

--- scripts/tragaperras.js
+++ scripts/tragaperras.js
@@ -1,8 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("load", function () {
+    //Cargar hora por primera vez
+    horaCargada = cargarHora();
+
+    //Cargar hora cada segundo
     setInterval(cargarHora, 1000);
+
+
+    // Ocultar el preloader
+    document.getElementById("preloader").style.display = "none";
+
+
 });
+
+
 function cargarHora() {
     var horaActual = new Date();
     var texto = document.getElementById('hora');
     texto.innerHTML = horaActual.getHours() + " : " + horaActual.getMinutes() + " : " + horaActual.getSeconds();
+    horaCargada = true;
+    return horaCargada;
 }
